11e7bcb664ca16730b5d0626c7181de4aa0123a3
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-03 08:38:50 +0300

    Add a grader function

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ if(number%2 == 0){
 }else{
     alert('The number is odd')
 };
+
+
+//Grader
+var score= parseInt(prompt('Enter your score: '));
+
+if (score>80 && score <=100 ){
+    alert('Grade A')
+}else if( score<=79 && score >60){
+    alert('Grade B')
+}else if(score <=59 && score >40){
+    alert('Grade C')
+}else if(score <=39 && score >30){
+    alert('Grade D')
+}else{
+    alert('Grade E')
+}
+
+
